4d2bbb63bdc861bb14bd1093a8ba78dfc859b7e8
Leonardo Perez <leono286@gmail.com>
2019-07-17 00:07:01 -0500

    feat: add open in a new window to download when resource is not in the same domain that crotalus

--- src/app/components/image-item-card/image-item-card.component.ts
+++ src/app/components/image-item-card/image-item-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-item-card',
   templateUrl: './image-item-card.component.html',
   styleUrls: ['./image-item-card.component.scss']
 })
-export class ImageItemCardComponent {
+export class ImageItemCardComponent implements OnInit {
 
   @Input() imageItem: any;
+  currentDomain: string;
+  resourceDomain: string;
 
   constructor() { }
+
+  ngOnInit() {
+    this.currentDomain = window.location.hostname;
+    this.resourceDomain = this.imageItem.about.split('://')[1].split('/')[0];
+  }
 
   getThumbnailUrl(): string {
     // return `${this.imageItem.about}/!/${this.imageItem.manifest.customicon}`;
@@ -22,6 +29,14 @@ export class ImageItemCardComponent {
 
   getResourceDownloadUrl(): string {
     return `${this.imageItem.about}/!/${this.imageItem.manifest.entrypoint}`;
+  }
+
+  openResourceInNewWindow(): void {
+    window.open(this.getResourceDownloadUrl(), '_blank');
+  }
+
+  get isResourceInSameDomain(): boolean {
+    return this.currentDomain === this.resourceDomain;
   }
 
 }
