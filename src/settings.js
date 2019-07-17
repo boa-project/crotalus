28e9a3a6ed5091865295660106747a45111b3609
Unknown <leono286@gmail.com>
2019-07-16 23:03:34 -0500

    update settings file

--- src/settings.js
+++ src/settings.js
@@ -1,12 +1,10 @@
 (function (window) {
   window.__appSettings = window.__appSettings || {};
 
-  // window.__appSettings.apiUri = 'https://boa.udem.edu.co/api';
-  window.__appSettings.apiUri = 'https://boa.udea.edu.co/api';
+  window.__appSettings.apiUri = 'https://boa.domain/api';
 
   window.__appSettings.catalogues = [
-    // { name: 'Banco de videos educativos de la UdeM', key: 'udem' }
-    { name: 'Banco de videos educativos de la UdeM', key: 'ruta-tic' }
+    { name: 'Catalogue name', key: 'catalogue-key' }
   ];
 
   window.__appSettings.filters = [
