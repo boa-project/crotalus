7e908b2076d063a99751b5b24e938d97170225f1
Leonardo Perez <leono286@gmail.com>
2021-02-21 22:17:37 -0500

    final changes made to wrap up update that enables to retrieve all type of content from repository. Dist files updated.

--- src/app/services/app-settings.service.ts
+++ src/app/services/app-settings.service.ts
@@ -3,14 +3,7 @@ export class AppSettings {
   // The values that are defined here are the default values that can
   // be overridden by settings.js
 
-  // API url
-  public apiUri = '';
-
   public repositories = [];
-
-  public filters = [
-    { meta: 'metadata.technical.format', value: ['video', 'audio', 'image', 'text', 'application'] }
-  ];
 
   public options = {
     resultsResponseSize: 20,
